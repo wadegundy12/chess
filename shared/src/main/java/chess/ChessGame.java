b040b88b6bdc937e146591e4ef7ad569e56634fc
wadegundy12 <wadegundy12@gmail.com>
2024-02-06 20:19:09 -0700

    all tests passed, gave up on bonus FINAL

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -15,12 +15,6 @@ public class ChessGame implements Cloneable{
     private TeamColor turn;
     private ChessBoard board;
 
-    private boolean bKingMoved;
-    private boolean wKingMoved;
-    private boolean wLeftRMoved;
-    private boolean bLeftRMoved;
-    private boolean wRightRMoved;
-    private boolean bRightRMoved;
 
 
 
@@ -30,7 +24,6 @@ public class ChessGame implements Cloneable{
         turn =TeamColor.WHITE;
         board = new ChessBoard();
         board.resetBoard();
-        setKingsAndRooks();
 
 
 
@@ -89,13 +82,6 @@ public class ChessGame implements Cloneable{
                     iterator.remove();
                 }
             }
-        }
-
-        if(board.getPiece(startPosition).getPieceType() == ChessPiece.PieceType.KING && turn == TeamColor.WHITE && !wKingMoved && !isInCheck(turn)){
-            moves.addAll(checkWhiteCastling(startPosition));
-        }
-        else if(board.getPiece(startPosition).getPieceType() == ChessPiece.PieceType.KING && !bKingMoved && !isInCheck(turn)){
-            moves.addAll(checkBlackCastling(startPosition));
         }
 
         return moves;
@@ -204,26 +190,9 @@ public class ChessGame implements Cloneable{
         return moves.isEmpty();
     }
 
-    public Collection<ChessMove> checkWhiteCastling(ChessPosition startPosition){
-        Collection<ChessMove> moves = new ArrayList<>();
-        ChessGame tempGame;
-        try {
-            tempGame = this.clone();
-        } catch (CloneNotSupportedException e) {
-            throw new RuntimeException(e);
-        }
-        if (!wLeftRMoved){
-            if (board.getPiece(new ChessPosition(1,4)) == null && board.getPiece(new ChessPosition(1,3)) == null) {
-
-            }
-        }
-
-    }
 
 
-    public Collection<ChessMove> checkBlackCastling(ChessPosition startPosition) {
 
-    }
 
         /**
          * Sets this game's chessboard with a given board
@@ -232,18 +201,6 @@ public class ChessGame implements Cloneable{
          */
     public void setBoard(ChessBoard board) {
         this.board = board;
-    }
-
-    private void setKingsAndRooks(){
-        wLeftRMoved = false;
-        bLeftRMoved = false;
-        wRightRMoved = false;
-        bRightRMoved = false;
-        wKingMoved = false;
-        bKingMoved = false;
-
-
-
     }
 
     /**
