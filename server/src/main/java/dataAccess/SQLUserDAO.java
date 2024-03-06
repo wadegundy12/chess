fcf349599bffe60c91112811b520a9e1d68f52b0
wadegundy12 <wadegundy12@gmail.com>
2024-03-06 15:49:07 -0700

    clear and createuser in USERDAO attempted

--- server/src/main/java/dataAccess/SQLUserDAO.java
+++ server/src/main/java/dataAccess/SQLUserDAO.java
@@ -2,10 +2,21 @@ package dataAccess;
 
 import model.UserData;
 
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Collection;
 
 public class SQLUserDAO implements UserDao {
+
+    final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  users (
+              username VARCHAR(50) PRIMARY KEY,
+              password VARCHAR(255) NOT NULL,
+              email VARCHAR(100) NOT NULL
+              INDEX(authtoken)
+            )
+            """
+    };
 
 
     public SQLUserDAO() throws DataAccessException {
@@ -14,11 +25,32 @@ public class SQLUserDAO implements UserDao {
 
     @Override
     public void clear() {
-
+        try(Connection connection = DatabaseManager.getConnection()){
+            String sql = "DELETE FROM users";
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate(sql);
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void createUser(UserData user) throws DataAccessException {
+        try(Connection connection = DatabaseManager.getConnection()) {
+            if (isUsernameTaken(connection, user.username())){
+                throw new DataAccessException("Error: already taken");
+            }
+            String sql = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setString(1, user.username());
+                statement.setString(2, user.password());
+                statement.setString(3, user.email());
+                statement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
@@ -37,16 +69,26 @@ public class SQLUserDAO implements UserDao {
         return null;
     }
 
-    public static void configureDatabase() throws DataAccessException {
+    public void configureDatabase() throws DataAccessException {
         DatabaseManager.createDatabase();
         try (var conn = DatabaseManager.getConnection()) {
-            for (var statement : createStatements) {
-                try (var preparedStatement = conn.prepareStatement(statement)) {
+            for (String statement : createStatements) {
+                try (PreparedStatement preparedStatement = conn.prepareStatement(statement)) {
                     preparedStatement.executeUpdate();
                 }
             }
         } catch (SQLException ex) {
             throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
+
+    private boolean isUsernameTaken(Connection conn, String username) throws SQLException {
+        String sql = "SELECT username FROM users WHERE username = ?";
+        try (PreparedStatement statement = conn.prepareStatement(sql)) {
+            statement.setString(1, username);
+            try (ResultSet rs = statement.executeQuery()) {
+                return rs.next();
+            }
         }
     }
 
--- server/src/main/java/dataAccess/UserDao.java
+++ server/src/main/java/dataAccess/UserDao.java
@@ -8,17 +8,7 @@ import java.util.Collection;
 
 public interface UserDao {
 
-    final String[] createStatements = {
-            """
-            CREATE TABLE IF NOT EXISTS  users (
-              username VARCHAR(50) NOT NULL,
-              password VARCHAR(255) NOT NULL,
-              email VARCHAR(100) NOT NULL,
-              authtoken VARCHAR(255) PRIMARY KEY
-              INDEX(authtoken)
-            )
-            """
-    };
+
     void clear();
     void createUser(UserData user) throws DataAccessException;
     UserData getUser(String username) throws DataAccessException;
