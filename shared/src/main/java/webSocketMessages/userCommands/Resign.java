8d0ddd0bb26deed6d531ea6694d64395e4473fea
wadegundy12 <wadegundy12@gmail.com>
2024-04-06 14:29:32 -0600

    Resign attempted

---
+++ shared/src/main/java/webSocketMessages/userCommands/Resign.java
@@ -0,0 +1,9 @@
+package webSocketMessages.userCommands;
+
+public class Resign extends UserGameCommand{
+    public int gameID;
+    public Resign(String authToken, int gameID) {
+        super(authToken);
+        this.gameID = gameID;
+    }
+}
